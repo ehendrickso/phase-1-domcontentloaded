02e920fa9ec50f0746365b43ff6090da01fe1d74
+ <erol.hendrickson@gmail.com>
2022-06-05 20:04:45 -0500

    commit tree 7bce5db5747e78f2cd1fdca4260304a83bc688cf

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+const newText = "This is really cool!"
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.body.innerHTML = document.body.innerHTML.replace('JavaScript is so cool. It lets me add text to my page programmatically.', "This is really cool!");
+  });
